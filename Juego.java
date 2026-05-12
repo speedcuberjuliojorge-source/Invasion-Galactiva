0d43d4c15e738dd7431a979d90d5a9601c1b9719
Julio Cruz <speedcuberjuliojorge@gmail.com>
2026-05-12 15:10:27 -0600

    Commit

--- Juego.java
+++ Juego.java
@@ -18,11 +18,14 @@ public class Juego {
     private Entrada teclado;
     private Destructor destructor;
     private int contaDestructor;
+    ElementoGrafico elemento[];
 
     //////////METODOS//////////
     //Constructor
     public Juego() throws InterruptedException {
 
+        //Declaraciones básicas
+        elemento = new ElementoGrafico[3];
         setContaDestructor(0);
         //Tamaño y escala lienzo
         canvas = new Lienzo();
@@ -32,22 +35,26 @@ public class Juego {
 
         //Establecer fondo
         fondo = new Fondo(canvas.pideLimiteXMax(), canvas.pideLimiteYMax());
+        elemento[0] = fondo;
 
         //Pintar Nave
         heroe = new NaveRebelde(canvas);
+        elemento[1] = heroe;
 
         //Mover Nave
         teclado = new Entrada();
 
-        //Pintar  Destructor
+        //Pintar Destructor
         destructor = new Destructor(canvas);
+        elemento[2] = destructor;
 
     }
 
     public void dibuja() {
-        canvas.dibujo((canvas.pideLimiteXMax() / 2), (canvas.pideLimiteYMax() / 2), fondo.getImgSprite(), 1000, 650);
-        canvas.dibujo(heroe.getX(), heroe.getY(), heroe.getImgSprite());
-        canvas.dibujo(destructor.getX(), destructor.getY(), destructor.getImgSprite());
+        canvas.dibujo((canvas.pideLimiteXMax() / 2), (canvas.pideLimiteYMax() / 2), elemento[0].getImgSprite(), 1000, 650);
+        for (int i = 1; i < elemento.length; i++) {
+            canvas.dibujo(elemento[i].getX(), elemento[i].getY(), elemento[i].getImgSprite());
+        }
         canvas.mostrar(0);
     }
 
@@ -78,16 +85,16 @@ public class Juego {
         System.out.println("\nx destructor: " + destructor.getX());
         System.out.println("Limite XMax: " + canvas.pideLimiteXMax());
         //if (canvas.fuePulsadaTecla(KeyEvent.VK_A)) {
-        if (destructor.getX() > (canvas.pideLimiteXMin() + 5) && derDestructor == true) {
+        if (destructor.getX() > (canvas.pideLimiteXMin() + 10) && derDestructor == true) {
             //sleep(600);//sleep(600) para que la nave recorra la pantalla en 5s
-            getTeclado().moverIzq(canvas, destructor, 10);
+            getTeclado().moverIzq(canvas, destructor, 20);
 
         } //}
         //if (canvas.fuePulsadaTecla(KeyEvent.VK_D)) {
         else if (destructor.getX() < (canvas.pideLimiteXMax() - 10)) {
             derDestructor = false;
             // sleep(600);//sleep(600) para que la nave recorra la pantalla en 5s
-            getTeclado().moverDer(canvas, destructor, 10);
+            getTeclado().moverDer(canvas, destructor, 20);
             if (!(destructor.getX() < (canvas.pideLimiteXMax() - 10))) {
                 derDestructor = true;
             }
